92ec98a3aaf41beecc06710ac685c16f359f74b3
brains1960 <tundunoladipo@gmail.com>
2018-07-16 14:03:17 -0400

    Express  and Socket Setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 import http from 'http';
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
+// Socket IO Setup
+server.listen(8080);
+io.on('connection', (socket) => {
+  socket.emit('msg', { hello: 'world' });
+  socket.on('cmd', (data) => {
+    console.log(data);
+  });
+});
+
+// Server Creation
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 export default class App extends React.Component {
+
+  componentDidMount() {
+    const socket = io('http://localhost:8080');
+    socket.on('connect', () => { console.log('ws connect'); });
+    socket.on('disconnect', () => { console.log('ws disconnect'); });
+    socket.on('msg', (data) => {
+      console.log('ws msg:', data);
+      socket.emit('cmd', { foo: 123 });
+    });
+  }
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
