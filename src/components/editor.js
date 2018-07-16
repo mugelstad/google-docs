2b0c1908c2a62597e633551a3c7e5bf80e9c0401
brains1960 <tundunoladipo@gmail.com>
2018-07-16 15:02:21 -0400

    Editor Setup + Installing draft-js (originally used draft.js)

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,14 +1,18 @@
 import React from 'react';
-
-// Components
-import MainEditor from './components/editor'
+import { Editor, EditorState } from 'draft-js';
 
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-export default class App extends React.Component {
-
+export default class MainEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onChange = editorState => this.setState({ editorState })
+  }
   componentDidMount() {
     const socket = io('http://localhost:8080');
     socket.on('connect', () => { console.log('ws connect'); });
@@ -19,9 +23,8 @@ export default class App extends React.Component {
     });
   }
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-      <MainEditor />
-    </div>);
+    return (
+      <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
   }
 }
